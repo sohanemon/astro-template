19c7c7ce0dac92425ff168b25a79ea6c731b496b
sohanemon <m.sohanemon@gmail.com>
2024-08-18 10:00:03 +0600

    chore: update configuration files and TypeScript settings feat: modify SEO component and RootLayout component in Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
+  site: process.env.CI
+    ? 'https://astro-template.vercel.app'
+    : 'http://localhost:3000',
   integrations: [tailwind(
     {
       applyBaseStyles: false,
