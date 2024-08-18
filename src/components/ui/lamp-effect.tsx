8a0766e250f5c15af02053eac5e71f3ba231a4d4
sohanemon <m.sohanemon@gmail.com>
2024-08-18 14:02:52 +0600

    chore(package.json): update dependencies style(src/components/ui/button.tsx): refactor code formatting refactor(src/components/ui/lamp-effect.tsx): improve lamp effect logic docs(src/pages/index.astro): update documentation for index page

--- src/components/ui/lamp-effect.tsx
+++ src/components/ui/lamp-effect.tsx
@@ -13,7 +13,7 @@ export function LampEffect() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+        className="mt-8 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight md:text-7xl"
       >
         Astro Template
       </motion.h1>
