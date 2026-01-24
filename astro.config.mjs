1034f588d528b0e929004021d6c9c92d86a16140
sohanemon <sohanemon@outlook.com>
2026-01-24 08:25:53 +0600

    style(astro.config): update import order and formatting feat(config): add biome configuration feat(script): add biome scripts to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: "static",
+  output: 'static',
   site: process.env.CI
-    ? "https://astromotion.vercel.app"
-    : "http://localhost:3000",
+    ? 'https://astromotion.vercel.app'
+    : 'http://localhost:3000',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,8 +1,9 @@
-import type { APIRoute } from "astro"
+import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = ({ params, request }) => {
-  return new Response(JSON.stringify({
-    message: "This was a GET!"
-  })
-  )
-}
+  return new Response(
+    JSON.stringify({
+      message: 'This was a GET!',
+    }),
+  );
+};
